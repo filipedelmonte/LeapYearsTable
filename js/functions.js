4342b2a25297aa3f688e03e3d68075b2346b8619
Filipe Lobo Del Monte <filipedelmonte@gmail.com>
2014-12-16 15:18:13 -0200

    Code improvement
    better performance

--- js/functions.js
+++ js/functions.js
@@ -1,43 +1,40 @@
 var randomArray = [];
+var N = 100;
 generateTableData = function(){
-    for(i = 0; i < 100; i++){
+    for(i = 0; i < N; i++){
     	randomArray[i] = Math.floor(Math.random() * 3000);
     	jQuery('.table > tbody:last').append("<tr><td style='font-weight: bold;' >"+(i+1)+"</td><td>"+randomArray[i]+"</td><td><i  id='result-icon-"+i+"' class='fa fa-spinner fa-spin' ></i></td></tr>");
     }
 }
-requestResults = function(){
-    for(j = 0; j < 100; j++){
+requestResults = function(index){
     	jQuery.ajax({
     		async: false,
-	        url: "http://localhost:8080/isleapyear/"+randomArray[j],
+	        url: "http://localhost:8080/isleapyear/"+randomArray[index],
 	        type: 'GET',
 	        dataType: 'html',
 	        success: function(response) {
-            	jQuery("#result-icon-"+j).removeClass();
-				jQuery("#result-icon-"+j).addClass("fa");
+            	jQuery("#result-icon-"+index).removeClass();
+				jQuery("#result-icon-"+index).addClass("fa");
 	            if(response == "true"){
-					jQuery("#result-icon-"+j).addClass("fa-check");
-					jQuery("#result-icon-"+j).addClass("green");
+					jQuery("#result-icon-"+index).addClass("fa-check");
+					jQuery("#result-icon-"+index).addClass("green");
 	            }
             	else{
-					jQuery("#result-icon-"+j).addClass("fa-remove");
-            		jQuery("#result-icon-"+j).addClass("red");
+					jQuery("#result-icon-"+index).addClass("fa-remove");
+            		jQuery("#result-icon-"+index).addClass("red");
             	}
 
 	        },
 	        error: function(response){
 	            console.log("Something went wrong!");
+            	jQuery("#result-icon-"+index).removeClass();
 	        }
 	    });
     }
-}
 
-start = function(){
-	generateTableData();
-	setTimeout(requestResults, 500);
-}
 
 jQuery( document ).ready(function() {
-	jQuery("#refresh-button").click(start);
-	start();
+	generateTableData();
+	for(i = 0; i < N; i++)
+		setTimeout(requestResults(i), 500);
 });
